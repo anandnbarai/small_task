acd335be2c8c0fe47458ad22c5be1027557a1040
Anand Barai <ab20892@gmail.com>
2023-10-08 14:23:59 +0530

    Add files via upload

--- Digital Clock/javascript.js
+++ Digital Clock/javascript.js
@@ -4,6 +4,10 @@ const secondsEl = document.getElementById("seconds")
 const ampmEl = document.getElementById("ampm")
 
 function updateClock() {
+
+    //! The new Date().getHours(), new Date().getMinutes(), and new Date().getSeconds() 
+    //! methods return the current hour, minute, and second, respectively.
+    
     let h = new Date().getHours()
     let m = new Date().getMinutes()
     let s = new Date().getSeconds()
@@ -14,6 +18,7 @@ function updateClock() {
         ampm = "PM"
     }
 
+    //! 
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
@@ -22,6 +27,8 @@ function updateClock() {
     minutesEl.innerText = m;
     secondsEl.innerText = s;
     ampmEl.innerText = ampm;
+
+    //! sets a timeout to call the updateClock() function every 1000 milliseconds (1 second).
     setTimeout(() => {
         updateClock()
     }, 1000)
